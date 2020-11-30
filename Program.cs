4bacf0d70702f72f3ac36f27c7d0f29a866ac491
Radi Kicheva <65068576+radostina92@users.noreply.github.com>
2020-11-30 19:58:21 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,63 @@
 ﻿using System;
 
-namespace Salary
+namespace ArabInRom
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int tab = int.Parse(Console.ReadLine());
-            int salary = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= tab; i++)
-            {
-                string name = Console.ReadLine();
-                switch (name)
-                {
-                    case "Facebook":
+            String result = "";
+            Console.Write("Enter Arabic number: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int thousands = i / 1000, hundreds = (i / 100) % 10, tens = (i / 10) % 10, ones = i % 10;
 
-                        salary -= 150;
-                        break;
-                    case "Instagram":
-                        salary -= 100;
-                        break;
-                    case "Reddit":
-                        salary -= 100;
-                        break;
-                }
-                if (salary <= 0)
-                {
-                    Console.WriteLine("You have lost your salary.");
-                    break;
-                }
-                
+            switch (thousands)
+            {
+                case 1: result += "M"; break;
+                case 2: result += "MM"; break;
+                case 3: result += "MMM"; break;
             }
-            if (salary > 0)
-            Console.WriteLine($"{salary}");
+
+            switch (hundreds)
+            {
+                case 1: result += "C"; break;
+                case 2: result += "CC"; break;
+                case 3: result += "CCC"; break;
+                case 4: result += "CD"; break;
+                case 5: result += "D"; break;
+                case 6: result += "DC"; break;
+                case 7: result += "DCC"; break;
+                case 8: result += "DCCC"; break;
+                case 9: result += "CM"; break;
+            }
+
+            switch (tens)
+            {
+                case 1: result += "X"; break;
+                case 2: result += "XX"; break;
+                case 3: result += "XXX"; break;
+                case 4: result += "XL"; break;
+                case 5: result += "L"; break;
+                case 6: result += "LX"; break;
+                case 7: result += "LXX"; break;
+                case 8: result += "LXXX"; break;
+                case 9: result += "XC"; break;
+            }
+
+            switch (ones)
+            {
+                case 1: result += "I"; break;
+                case 2: result += "II"; break;
+                case 3: result += "III"; break;
+                case 4: result += "IV"; break;
+                case 5: result += "V"; break;
+                case 6: result += "VI"; break;
+                case 7: result += "VII"; break;
+                case 8: result += "VIII"; break;
+                case 9: result += "IX"; break;
+            }
+
+            Console.WriteLine("Roman number is " + result);
         }
     }
 }
---
+++ averageNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class averageNumber
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum = sum + num;
+            }
+            double average = sum / n;
+            Console.WriteLine($"{average:f2}");
+        }
+    }
+}
