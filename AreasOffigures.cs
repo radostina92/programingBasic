dac75d056d61a0472193d0378dd8fb09ddfed9e1
Radi Kicheva <65068576+radostina92@users.noreply.github.com>
2020-11-30 18:46:01 +0200

    Add files via upload

---
+++ AreasOffigures.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AreasOffigures
+{
+    class AreasOffigures
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double side = double.Parse(Console.ReadLine());
+            if (figure == "square")
+            {
+                Console.WriteLine(side * side);
+            }
+            else if (figure == "rectangle")
+            {
+                double height = double.Parse(Console.ReadLine());
+                Console.WriteLine(side * height);
+            }
+            else if (figure == "circle")
+            {
+                double pi = Math.PI; 
+                Console.WriteLine("{0:f3}",pi * side * side);
+            }
+            else if (figure == "triangle")
+            {
+                double height2 = double.Parse(Console.ReadLine());
+                Console.WriteLine( (side * height2) / 2);
+            }
+        }
+    }
+}
