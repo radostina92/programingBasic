0c8b675f7445c1588e1f3df4e962ac21978dd534
Radi Kicheva <65068576+radostina92@users.noreply.github.com>
2020-11-30 19:48:56 +0200

    Add files via upload

---
+++ CleverLilly.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CleverLilly
+{
+    class CleverLilly
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washPrice = double.Parse(Console.ReadLine());
+            double toyPrice = double.Parse(Console.ReadLine());
+            double bdaysMoney = 0;
+            double moneyGift = 10.00;
+            double toys = 0;
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    bdaysMoney += moneyGift;
+                    moneyGift += 10;
+                    bdaysMoney--;
+                }
+                else
+                {
+                    toys++;
+                }
+
+            }
+            double totalMoneyToys = toys * toyPrice;
+            double totalMoney = totalMoneyToys + bdaysMoney;
+            
+            if (totalMoney >= washPrice)
+            {
+                Console.WriteLine($"Yes! {totalMoney - washPrice:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washPrice - totalMoney:f2}");
+            }
+        }
+    }
+}
---
+++ DelenieBezOst.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DelenieBezOst
+{
+    class DelenieBezOst
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            double sum3 = 0;
+            double sum4 = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                if (a % 2 == 0)
+                {
+                    sum++;
+                }
+                if (a % 3 == 0)
+                {
+                    sum3++;
+                }
+                if (a % 4 == 0)
+                {
+                    sum4++;
+                }
+            }
+            double cnt2 = sum  / n * 100;
+            double cnt3 = sum3 / n * 100;
+            double cnt4 = sum4 / n * 100;
+
+            Console.WriteLine($"{cnt2:f2}%");
+            Console.WriteLine($"{cnt3:f2}%");
+            Console.WriteLine($"{cnt4:f2}%");
+
+        }
+    }
+}
