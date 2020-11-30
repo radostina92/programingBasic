8f8a918f1c0aab81234e3af9103d99eb4e9db86a
Radi Kicheva <65068576+radostina92@users.noreply.github.com>
2020-11-30 18:47:22 +0200

    Add files via upload

---
+++ CharacterSequences.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CharacterSequences
+{
+    class CharacterSequences
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
